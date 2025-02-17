a705ae53c6246d301c3cd55d240c313f561e3e30
Leo B <lszborges@gmail.com>
2025-02-17 16:46:26 -0300

    Changed db config

--- src/config/database.js
+++ src/config/database.js
@@ -1,30 +1,14 @@
 import "dotenv/config";
 
 export default {
-  development: {
-    username: process.env.DB_DEV_USERNAME,
-    password: process.env.DB_DEV_PASSWORD,
-    database: process.env.DB_DEV_DATABASE,
-    host: process.env.DB_DEV_HOST,
-    dialect: process.env.DB_DEV_DIALECT,
-    define: {
-      timestamps: process.env.DB_DEV_DEFINE_TIMESTAMPS === "true",
-      underscored: process.env.DB_DEV_DEFINE_UNDERSCORED === "true",
-      underscoredAll: process.env.DB_DEV_DEFINE_UNDERSCOREDALL === "true",
-    },
-  },
-  test: {
-    username: process.env.DB_TEST_USERNAME,
-    password: process.env.DB_TEST_PASSWORD,
-    database: process.env.DB_TEST_DATABASE,
-    host: process.env.DB_TEST_HOST,
-    dialect: process.env.DB_TEST_DIALECT,
-  },
-  production: {
-    username: process.env.DB_PROD_USERNAME,
-    password: process.env.DB_PROD_PASSWORD,
-    database: process.env.DB_PROD_DATABASE,
-    host: process.env.DB_PROD_HOST,
-    dialect: process.env.DB_PROD_DIALECT,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  host: process.env.DB_HOST,
+  dialect: process.env.DB_DIALECT,
+  define: {
+    timestamps: process.env.DB_DEFINE_TIMESTAMPS === "true",
+    underscored: process.env.DB_DEFINE_UNDERSCORED === "true",
+    underscoredAll: process.env.DB_DEFINE_UNDERSCOREDALL === "true",
   },
 };
