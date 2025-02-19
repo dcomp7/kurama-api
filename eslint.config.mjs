3ee8a87b224c2dc4562da8bdb36fe9f0dbc7d819
Leo B <lszborges@gmail.com>
2025-02-19 17:33:20 -0300

    Adjusted export

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,7 +3,6 @@ import pluginJs from "@eslint/js";
 import prettier from "eslint-config-prettier";
 import prettierPlugin from "eslint-plugin-prettier";
 
-/** @type {import('eslint').Linter.FlatConfig[]} */
 export default [
   {
     languageOptions: {
